ae088d751452ce157cafbf2b7d5ccfa1751066c9
JP Navarro <jpfnavarro@gmail.com>
2016-12-18 20:03:18 +0000

    Upgrade to Python 2.7.13-1

--- sbin/route_glue2.sh
+++ sbin/route_glue2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export PYTHONPATH=/soft/warehouse-apps-1.0/Manage-Glue2/PROD
-export LD_LIBRARY_PATH=/soft/python-2.7.12-1/lib
-/soft/python-2.7.12-1/bin/python /soft/warehouse-apps-1.0/Manage-Glue2/PROD/bin/route_glue2.py ${@:1}
+export LD_LIBRARY_PATH=/soft/python-2.7.13-1/lib
+/soft/python-2.7.13-1/bin/python /soft/warehouse-apps-1.0/Manage-Glue2/PROD/bin/route_glue2.py ${@:1}
