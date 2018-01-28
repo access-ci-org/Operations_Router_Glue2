88bc81312ca8c72b90f97467a301f1699b97778c
JP Navarro <jpfnavarro@gmail.com>
2018-01-28 14:18:24 +0000

    Fix: connect to info.db.xsede.org instead of localhost and rely on ~/.pgpass

--- bin/cleanup.sh
+++ bin/cleanup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+# DB connection relies on ~/.pgpass
 
 function main_function
 {
    cd /soft/warehouse-apps-1.0/Manage-Glue2/PROD/
-   time psql warehouse glue2_owner <bin/cleanup.sql
+   time psql warehouse glue2_owner -h infodb.xsede.org <bin/cleanup.sql
 }
 
 main_function 2>&1 | mail -s "GLUE2 weekly cleanup" "navarro@mcs.anl.gov"
