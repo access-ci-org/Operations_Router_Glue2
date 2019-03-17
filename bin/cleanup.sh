623c2bf9440338f8e98c198cd97c4e15b3ff8162
JP Navarro <jpfnavarro@gmail.com>
2019-03-17 17:53:52 +0000

    Point to RDS server

--- bin/cleanup.sh
+++ bin/cleanup.sh
@@ -4,7 +4,7 @@
 function main_function
 {
    cd /soft/warehouse-apps-1.0/Manage-Glue2/PROD/
-   time psql warehouse glue2_owner -h infodb.xsede.org <bin/cleanup.sql
+   time psql warehouse glue2_owner -h information-warehouse-prod-cluster.cluster-clabf5kcvwmz.us-east-2.rds.amazonaws.com <bin/cleanup.sql
 }
 
 main_function 2>&1 | mail -s "GLUE2 weekly cleanup" "navarro@mcs.anl.gov"
