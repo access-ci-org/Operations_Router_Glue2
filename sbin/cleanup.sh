aece91a9a51a17012dfd0758903c4d7eedae396c
John-Paul Navarro <navarro@anl.gov>
2024-11-01 11:27:05 -0500

    Disable sbin/cleanup.sh email, fix DB_HOSTNAME

--- sbin/cleanup.sh
+++ sbin/cleanup.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 # DB connection relies on ~/.pgpass
 APP_HOME=%APP_HOME%
-DB_HOSTNAME=%DB_HOSTNAME_WRITE%
+DB_HOSTNAME=%DB_HOSTNAME%
 
 function main_function
 {
    cd ${APP_HOME}
-   time psql warehouse2 info_django -h ${DB_HOSTNAME} <bin/cleanup.sql
+   time psql warehouse2 info_django -h ${DB_HOSTNAME} <PROD/bin/cleanup.sql
 }
 
-main_function 2>&1 | tee -a cleanup.log | mail -r ops-support-notify@access-ci.org -s "`hostname`: GLUE2 weekly cleanup" "ops-support-notify@access-ci.org"
+# Future pipe to mail
+# | mail -r ops-support-notify@access-ci.org -s "`hostname`: GLUE2 weekly cleanup" "ops-support-notify@access-ci.org"
+main_function 2>&1 >>var/cleanup.log
