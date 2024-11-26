50fe08e034d95073802610092d50aafa54c3c3b4
John-Paul Navarro <navarro@anl.gov>
2024-11-26 07:02:24 -0600

    Fix cleanup.sh to cd to APP_HOME sooner, add opsapi2 to cache ping

--- sbin/cleanup.sh
+++ sbin/cleanup.sh
@@ -5,11 +5,13 @@ DB_HOSTNAME=%DB_HOSTNAME%
 
 function main_function
 {
-   cd ${APP_HOME}
    echo "** Starting at `date --rfc-3339=ns`"
    psql warehouse2 info_django -h ${DB_HOSTNAME} <PROD/bin/cleanup.sql
    echo "** Ending at `date --rfc-3339=ns`"
 }
+
+# So that we can use APP_HOME relative paths in this script
+cd ${APP_HOME}
 
 # Future pipe to mail
 # | mail -r ops-support-notify@access-ci.org -s "`hostname`: GLUE2 weekly cleanup" "ops-support-notify@access-ci.org"
--- sbin/ping_cache.sh
+++ sbin/ping_cache.sh
@@ -2,13 +2,15 @@
 # Description:       Touch cache to keep it fresh
 
 APP_BASE=/soft/applications-2.0/router_glue2
-CACHE_URL=https://opsapi1.access-ci.org/wh1/warehouse-views/v1/software-cached/
+CACHE1_URL=https://opsapi1.access-ci.org/wh1/warehouse-views/v1/software-cached/
+CACHE2_URL=https://opsapi1.access-ci.org/wh1/warehouse-views/v1/software-cached/
 PING_LOG=${APP_BASE}/var/ping_cache.log
 
 do_ping () {
     echo -n "Ping at: "
     date
-    /usr/bin/time -f "%E" wget -q -O /dev/null ${CACHE_URL}
+    /usr/bin/time -f "%E" wget -q -O /dev/null ${CACHE1_URL}
+    /usr/bin/time -f "%E" wget -q -O /dev/null ${CACHE2_URL}
 }
 
 do_ping >>${PING_LOG} 2>&1
